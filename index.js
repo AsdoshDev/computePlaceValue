ec7b7624554e0574eb0609519640ace503811069
AsdoshDev <karthikdev27@gmail.com>
2019-05-16 16:26:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-function computePlaceValue(x, arr) {
+function computePlaceValue(x, array) {
     let numString = x ? x.toString() : "";
-    var arr = (arr && arr.length > 0) ? arr : [];
     let noOfZeros = (numString).length - 1;
     let placeValue = 1;
+    let arr = array;
+    arr = (arr && arr.length > 0) ? arr : [];
     for (let i = 0; i < noOfZeros; i++) {
       placeValue = placeValue * 10;
     }
     let reduceValue = (Math.floor(x / placeValue)) * placeValue;
     arr.push(reduceValue);
-    if (numString.length > 1) {
+    if(numString.length > 1)
       computePlaceValue(x % placeValue, arr);
-    }
-    else {
-      console.log(arr);
-      return arr;
-    }
-  }
+    return arr;
+}  
   
 // examples 
 console.log(computePlaceValue(83913,[]));
